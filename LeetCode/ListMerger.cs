f3fa17f245673f9cfddd6605e780cb0cfdbbbde4
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-05 20:32:07 +0300

    List Merger added

---
+++ LeetCode/ListMerger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace LeetCode
+{
+	public class ListMerger
+	{
+		public ListNode MergeTwoLists(ListNode l1, ListNode l2) 
+		{
+			ListNode head1 = l1;
+			ListNode head2 = l2;
+
+			ListNode newHead = null;
+			ListNode newLast = null;
+
+			while (head1 != null && head2 != null) 
+			{
+				if (head1.val < head2.val) {
+					if (newHead == null) {
+						newHead = head1;
+						newLast = head1;
+					} else {
+						newLast.next = head1;
+						newLast = newLast.next;
+					}
+					head1 = head1.next;
+				} else {
+					if (newHead == null) {
+						newHead = head2;
+						newLast = head2;
+					} else {
+						newLast.next = head2;
+						newLast = newLast.next;
+					}
+					head2 = head2.next;
+				}
+			}
+
+			if (head1 != null && head2 == null) {
+				if (newHead == null) {
+					newHead = head1;
+					newLast = head1;
+				} else {
+					newLast.next = head1;
+				}
+			}else if(head1 == null && head2 != null){
+				if (newHead == null) {
+					newHead = head2;
+					newLast = head2;
+				} else {
+					newLast.next = head2;
+				}
+			}
+
+			return newHead;
+		}
+
+	}
+}
+
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -18,35 +18,39 @@ namespace LeetCode
 //				{'5','.','.','.','.','.','.','.','.'}
 //			};
 
-			ListNode n1 = new ListNode (3);
-			ListNode n2 = new ListNode (2);
-			ListNode n3 = new ListNode (0);
-			ListNode n4 = new ListNode (-4);
-			ListNode n5 = new ListNode (1);
-			ListNode n6 = new ListNode (22);
-			ListNode n7 = new ListNode (-1);
-			ListNode n8 = new ListNode (-2);
-			ListNode n9 = new ListNode (-3);
-			ListNode n10 = new ListNode (4);
-			ListNode n11 = new ListNode (-5);
-			ListNode n12 = new ListNode (-6);
+			ListNode n1 = new ListNode (1);
+			ListNode n2 = new ListNode (5);
+			ListNode n3 = new ListNode (10);
+			ListNode n4 = new ListNode (15);
+
+			ListNode n7 = new ListNode (2);
+			ListNode n8 = new ListNode (3);
+			ListNode n9 = new ListNode (7);
+			ListNode n10 = new ListNode (12);
+			ListNode n11 = new ListNode (16);
+			ListNode n12 = new ListNode (32);
 
 			n1.next = n2;
 			n2.next = n3;
 			n3.next = n4;
-			n4.next = n5;
-			n5.next = n6;
-			n6.next = n7;
+
 			n7.next = n8;
 			n8.next = n9;
 			n9.next = n10;
 			n10.next = n11;
 			n11.next = n12;
-			n12.next = n6;
 
-			Cycle c = new Cycle ();
 
-			Console.WriteLine (c.DetectCycle(n1).val);
+			ListNode specialNode = new ListNode (0);
+
+			ListMerger m = new ListMerger ();
+			var start = m.MergeTwoLists (null, specialNode);
+
+			while (start != null) {
+				Console.WriteLine (start.val);
+				start = start.next;
+			}
+
 		}
 	}
 }
