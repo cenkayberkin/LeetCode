61167b5b3a8804c39057a95f0b94864493d8d9c1
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-05 17:09:38 +0300

    Remove nth node from end

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -17,9 +17,22 @@ namespace LeetCode
 //				{'6','.','8','9','7','.','.','3','.'},
 //				{'5','.','.','.','.','.','.','.','.'}
 //			};
-			RomanAlph r = new RomanAlph();
 
-			Console.WriteLine (r.RomanToInt ("MDLXX"));
+			ListNode n1 = new ListNode (1);
+			ListNode n2 = new ListNode (2);
+			ListNode n3 = new ListNode (3);
+			ListNode n4 = new ListNode (4);
+			ListNode n5 = new ListNode (5);
+
+
+			RemoveNthNode r = new RemoveNthNode ();
+
+			var head = r.RemoveNthFromEnd (n1, 1);
+			while (head != null) {
+				Console.WriteLine (head.val);
+				head = head.next;
+			}
+		
 		}
 	}
 }
---
+++ LeetCode/RemoveNthNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode
+{
+	public class ListNode 
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int x) { val = x; }
+	}
+ 
+	public class RemoveNthNode
+	{
+		public ListNode RemoveNthFromEnd(ListNode head, int n) 
+		{	
+			var firstPointer = head;
+			var secondPointer = head;
+
+			int i = 0;
+			while (i < n) {
+				firstPointer = firstPointer.next;
+				i += 1;
+			}
+
+			if (firstPointer == null) {
+				return head.next;
+			}
+
+			while (firstPointer.next != null) {
+				firstPointer = firstPointer.next;
+				secondPointer = secondPointer.next;
+			}
+
+			secondPointer.next = secondPointer.next.next;
+
+			return head;
+		}
+	}
+}
+
