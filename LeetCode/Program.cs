46f4e3fbb5aa3253a2cb905e7b6d731b01efe8f9
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-08 13:54:30 +0300

    Sqrt solution changed

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -20,25 +20,29 @@ namespace LeetCode
 //				{'5','.','.','.','.','.','.','.','.'}
 //			};
 //
-			ListNode n1 = new ListNode(1);
-			ListNode n2 = new ListNode(2);
-			ListNode n3 = new ListNode(3);
-			ListNode n4 = new ListNode(4);
-			ListNode n5 = new ListNode(5);
+//			ListNode n1 = new ListNode(1);
+//			ListNode n2 = new ListNode(2);
+//			ListNode n3 = new ListNode(3);
+//			ListNode n4 = new ListNode(4);
+//			ListNode n5 = new ListNode(5);
+//
+//			n1.next = n2;
+//			n2.next = n3;
+//			n3.next = n4;
+//			n4.next = n5;
+//
+//			ListOperations o = new ListOperations ();
+//			var head = o.ReverseListRecursive (n1);
+//
+//			var tmp = head;
+//			while (tmp != null) {
+//				Console.WriteLine (tmp.val);
+//				tmp = tmp.next;
+//			}
 
-			n1.next = n2;
-			n2.next = n3;
-			n3.next = n4;
-			n4.next = n5;
-
-			ListOperations o = new ListOperations ();
-			var head = o.ReverseListRecursive (n1);
-
-			var tmp = head;
-			while (tmp != null) {
-				Console.WriteLine (tmp.val);
-				tmp = tmp.next;
-			}
+			Squareroot r = new Squareroot ();
+			Console.WriteLine (r.MySqrt (120));
+		
 		}
 	}
 }
--- LeetCode/Squareroot.cs
+++ LeetCode/Squareroot.cs
@@ -7,31 +7,25 @@ namespace LeetCode
 		public int MySqrt(int x) 
 		{
 			if (x == 0) { return 0; }
-			if (x >= 1 && x < 3) { return 1; }
-			if (x >= 3 && x < 9) { return 2; }
+			if (x >= 1 && x <= 3) { return 1; }
+			if (x >= 4 && x < 9) { return 2; }
 			if (x >= 9 && x < 16) { return 3; }
 			if (x >= 16 && x < 25) { return 4; }
 			if (x >= 25 && x < 36) { return 5; }
 
 			int length = x / 5;
-			int[] candidates = new int[length + 1];
 
-			for (int i = 0; i < length + 1; i++) 
-			{
-				candidates [i] = i;	
-			}
-
-			return findSqrt (candidates, x, 0, length);
+			return findSqrt (x, 0, length);
 		}
 
-		public int findSqrt(int[] candidates,int x,int lo, int hi)
+		public int findSqrt(int x,int lo, int hi)
 		{
 			if (lo == hi) { return 0; }
 			int mid = lo + (hi - lo) / 2;
-			double pow = Math.Pow (candidates [mid], 2);
+			double pow = Math.Pow (mid, 2);
 
-			double lowDiff = pow - (candidates [mid - 1] + candidates [mid]);
-			double upperDiff = pow + (candidates [mid] + candidates [mid + 1]);
+			double lowDiff = pow - ((mid - 1) + mid);
+			double upperDiff = pow + (mid + mid + 1);
 
 			if (x == pow || (x > pow && x < upperDiff)){
 				return mid;
@@ -42,9 +36,9 @@ namespace LeetCode
 			}
 
 			if (pow < x) {
-				return findSqrt (candidates, x, mid + 1, hi);
+				return findSqrt (x, mid + 1, hi);
 			} else {
-				return findSqrt (candidates, x, lo, mid);
+				return findSqrt (x, lo, mid);
 			}
 		
 		}
